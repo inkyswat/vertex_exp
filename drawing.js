2f03d207f06a59b570c797c83f75d56dd074d6e7
Oliver Roots <inkyswat@tlu.ee>
2018-04-22 21:05:53 +0300

    init commit

---
+++ drawing.js
@@ -0,0 +1,201 @@
+var ctx;
+let PHI = (1 + Math.sqrt(5)) / 2;
+var getCanvas = function(){
+    var canvas = document.createElement("canvas");
+
+    canvas.id = "cursorLayer";
+    canvas.width = 1224;
+    canvas.height = 768;
+    canvas.style.zIndex = 8;
+    canvas.style.position = "absolute";
+    canvas.style.border = "1px solid black";
+
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(canvas);
+    var ctx = canvas.getContext("2d");
+    return ctx;
+    }
+window.onload = function(){
+    
+    ctx = getCanvas();
+    ctx.beginPath();
+    ctx.fillstyle = "rgba(255, 0, 0, 0.2)";
+    var spiral = new Spiral(225, 0.078);      
+}
+class Spiral{
+    
+    constructor(length, pace){
+        this.length = length;
+        this.pace = pace;
+        this.ctx = getCanvas();
+        this.ctx.clearRect(0, 0, 1224, 768);
+        this.spiral = this.getPath();
+        let spiral2 = Spiral.translateEnd(this.spiral);
+        Spiral.tree(spiral2, 600, 400);
+        //Spiral.drawTree(spiral2, 600, 400, 26);
+    }
+    
+    getPath(){
+        var spiralPath = [];
+        let r = Math.pow(PHI, 0);
+        let nX = 0;
+        let nY = -r;
+        spiralPath[0] = new Vertex(nX, nY, 0);
+        for(let i = 1; i < this.length; i++){
+            let phi = i * this.pace;
+            let r = Math.pow(PHI, (phi) * 2 / Math.PI);
+            let newX = Math.sin(phi) * r;
+            let newY = -Math.cos(phi) * r;
+            spiralPath[i] = new Vertex(newX, newY, 0);    
+        }
+        return spiralPath;
+    }
+   
+    static drawSpiral(spiralArray, size, x, y){
+        ctx.beginPath();
+        ctx.moveTo(spiralArray[0].x + x, spiralArray[0].y + y);
+        for(let i = 1; i < spiralArray.length; i++){
+            ctx.lineTo(x + spiralArray[i].x, y + spiralArray[i].y);
+            ctx.stroke();
+        }
+    }
+    
+    static translateEnd(V)
+        {
+            let B = [];
+            let v = new Vertex(V[V.length-1].x, V[V.length-1].y,V[V.length-1].z);
+            let transX = v.x;
+            let transY = v.y;
+            let transZ = v.z;
+            for (let i = 0; i < V.length - 1; i++){
+                B[i] = new Vertex(V[i].x - transX, V[i].y - transY, V[i].z - transZ);
+            }
+            B[V.length -1] = new Vertex(0,0,0);
+            return B;
+        }
+    
+    static translate( vertex, vector){
+        return new Vertex(vertex.x - vector.x, vertex.y - vector.y, 0);
+    }
+    static rotate(V, angle)
+    {
+        let B = [];
+        for (let i = 0; i < V.length - 1; i++){
+                B[i] = V[i].rotZ(angle);
+            }
+        return B;
+    }
+    static drawTree(V, xx, yy, pace){
+        var U = [];
+        U = Spiral.mirror(V);
+        var left = function(cursor, x, y){ 
+            ctx.moveTo(x, y);
+            var newX;   // on linemap
+            var newY;   // on linemap
+            let translation = new Vector(V[cursor].x, V[cursor].y);
+            for(let i = cursor; i > 0 && i > cursor - pace; i--){
+                let point = Spiral.translate(V[i], translation);
+                newX = x + point.x;
+                newY = y + point.y;
+                ctx.lineTo(xx + newX, yy + newY);
+                ctx.stroke();
+            }
+            if(cursor > pace)
+            {
+                left(cursor - pace, newX, newY);
+                right(cursor - pace, newX, newY);
+            }
+        }
+        var right = function(cursor, x, y){
+            ctx.moveTo(x, y);
+            var newX;   // on linemap
+            var newY;   // on linemap
+            let translation = new Vector(V[cursor].x, V[cursor].y);
+            let p1 = new Vector(V[cursor].x, V[cursor].y);
+            let p2 = new Vector(V[cursor - 1].x, V[cursor - 1].y);
+            let vector1 = Vector.getVector(p1, p2);
+            let angle1 = Vector.getAngle(vector1);
+            let p3 = new Vector(U[cursor].x, U[cursor].y);
+            let p4 = new Vector(U[cursor - 1].x, U[cursor - 1].y);
+            let vector2 = Vector.getVector(p3, p4);
+            let angle2 = Vector.getAngle(vector2);
+            let disc = angle1 - angle2;
+            for(let i = cursor; i > 0 && i > cursor - pace; i--){
+                let point = Spiral.translate(V[i], translation).rotZ(disc);
+                newX = x + point.x;
+                newY = y + point.y;
+                ctx.lineTo(xx + newX, yy + newY);
+                ctx.stroke();
+            }
+            if(cursor > pace)
+            {
+                left(cursor - pace, newX, newY);
+                right(cursor - pace, newX, newY);
+            }
+        }
+        left(V.length - 1, 0, 0);
+        right(V.length - 1, 0, 0);
+    }
+    static tree(V, x, y)
+    {
+        var P = [];
+        var U = [];
+        var p = 26
+        if(V.length > p){
+            let a = V.length;
+            for(let i = 0; i < V.length - p; i++){
+                P[i] = V[i];
+            }
+            let xx = V[V.length - p].x;
+            let yy = V[V.length - p].y;
+            let p1 = new Vector(V[V.length - p].x, V[V.length - p].y);
+            let p2 = new Vector(V[V.length - (p+1)].x, V[V.length - (p+1)].y);
+            let vector1 = Vector.getVector(p1, p2);
+            let angle1 = Vector.getAngle(vector1);
+            P = Spiral.translateEnd(P);
+            U = Spiral.mirror(P);
+            let p3 = new Vector(U[U.length - 1].x, U[U.length - 1].y);
+            let p4 = new Vector(U[U.length - 2].x, U[U.length - 2].y);
+            let vector2 = Vector.getVector(p3, p4);
+            let angle2 = Vector.getAngle(vector2); 
+            let disc = angle1 - angle2;
+            U = Spiral.rotate(U, disc);
+            Spiral.tree(P, x + xx, y + yy);
+            Spiral.tree(U, x + xx, y + yy);
+        }
+        Spiral.drawSpiral(V, 1, x, y)
+    }
+    
+    static mirror(V){
+        var M = [];
+        for(let i = 0; i < V.length; i++){
+            M[i] = new Vertex(-V[i].x, V[i].y, 0);
+        }
+        return M;
+    }
+}
+
+class Vector{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    static getVector(p1, p2){
+        let x = p2.x - p1.x;
+        let y = p2.y - p1.y;
+        return new Vector(x, y);
+    }
+    
+    static getAngle(vector){
+        return Math.atan2(vector.y, vector.x);
+    }
+    scaleToOne(){
+        let a = 1 / Math.sqrt(this.x * this.x + this.y * this.y);
+        return new Vector(a * this.x, a * this.y);
+    }
+    
+    normal(){
+        return new Vector(-this.y, this.x);
+    }
+}
---
+++ vertex.js
@@ -0,0 +1,232 @@
+class Vertex
+    {
+        static rad(){return 0.017453292519943295};//Math.PI / 180};
+        static custom(P)
+        {
+             if (typeof(P.x) === 'undefined')
+            {
+                return {x:P[0], y:P[1], z:P[2]};
+            }
+            else 
+            {
+                return P;
+            }
+        }
+        static add(v1, v2)
+        {
+            let V1 = Vertex.custom(v1),
+                V2 = Vertex.custom(v2);    
+            //return new Vertex(V1.x + V2.x, V1.y + V2.y, V1.z + V2.z);
+            return {x: V1.x + V2.x, y: V1.y + V2.y, z: V1.z + V2.z};
+        }
+        static divide(V1, V2)
+            {
+                let A = Vertex.custom(V1),
+                    B = Vertex.custom(V2);
+                return {x: A.x / B.x, y: A.y / B.y, z: A.z / B.z};
+            }
+        static vector(beginP,endP)
+        {
+            let a = Vertex.custom(beginP), 
+                b = Vertex.custom(endP);
+            
+            return {x: b.x - a.x ,y: b.y - a.y ,z: b.z - a.z};
+        }
+        
+        static translateEnd(V)
+        {
+            /*
+            let B = [];
+            let v = new Vertex(V[V.length-1].x, V[V.length-1].y,V[V.length-1].z);
+            let transX = v.x;
+            let transY = v.y;
+            let transZ = v.z;
+            for (i = 0; i < V.length - 1; i++){
+                B[i] = new Vertex(V[i].x - transX, V[i].y - transY, V[i].z - transZ);
+            }
+            B[V.length -1] = new Vertex(0,0,0);
+            return B;
+            */
+        }
+        
+        static cAngle(p1, p2)
+        {
+            let v = Vertex.scale(0.5,Vertex.vector(p1, p2));
+            let ll = v.x * v.x + v.y * v.y + v.z * v.z;
+            return 2 * Math.atan(Math.sqrt(ll / 3));
+        }
+        static scale(a, p)
+        {
+            let P = Vertex.custom(p);
+            return new Vertex(P.x * a, P.y * a, P.z * a);
+        }
+        static  midpoint(PolyV)
+        {
+            let x = PolyV[0].x,
+                y = PolyV[0].y,
+                z = PolyV[0].z;
+            let i;
+            for(i = 1; i < PolyV.length; i++)
+            {
+                x += PolyV[i].x,
+                y += PolyV[i].y,
+                z += PolyV[i].z;
+            }
+            //return {x : x / i, y : y / i, z : z / i};
+            return new Vertex(x / i, y / i, z / i);
+        }
+        static shift2(Parr, Pref)
+        {
+            let convey = [];
+            for(let i = 0; i < Parr.length; i++)
+            {
+                convey[i] = {x : Parr[i].x - Pref.x, y : Parr[i].y - Pref.y, z : Parr[i].z - Pref.z};   
+            }
+            return convey;
+        }
+        static monoscale(Arr, x)
+        {
+            let convey = [];
+            for(let i = 0; i < Arr.length; i++)
+            {
+                convey[i] = {x : Arr[i].x * x, y : Arr[i].y * x, z : Arr[i].z * x};   
+            }
+            return convey;
+        }
+        static dist(A, B)
+        {
+            let V;
+            if(typeof(B) === 'undefined')
+            {
+                V = Vertex.custom(A);
+            }
+            else
+            {
+                V = Vertex.vector(A, B);
+            }
+            return Math.sqrt(V.x * V.x + V.y * V.y + V.z * V.z);
+        }
+        static normal(v1,v2)// NORMAL to two vectors
+            { //third point is 0,0,0 - both vectors there
+                let a = v1.y * v2.z - v1.z * v2.y;
+                let b = -(v1.x * v2.z - v1.z * v2.x);
+                let c = v1.x * v2.y - v1.y * v2.x;
+                return {x:a,y:b,z:c};
+            }
+        static plane(p1, p2, p3)// PLANE parameters by 3 points
+            {
+                let V1 = Vertex.vector(p1, p2);//{x:p2.x - p1.x, y:p2.y - p1.y, z:p2.z - p1.z};
+                let V2 = Vertex.vector(p1, p3);//{x:p3.x - p1.x, y:p3.y - p1.y, z:p3.z - p1.z};
+                let N = Vertex.normal(V1, V2);
+                return N;
+            }
+        static tangent(N, p1, l1)
+            {
+                let num = N.x * p1.x + N.y * p1.y + N.z * p1.z;
+                let den = N.x * l1.x + N.y * l1.y + N.z * l1.z;
+                let t = num / den;
+                return t;
+            }
+        static planeInt(Plane, A, B)//INTERSECTION line parametric form
+            {
+                let beam = Vertex.vector(A,B);
+                let num = Plane.x * A.x + Plane.y * A.y + Plane.z * A.z;
+                let den = Plane.x * beam.x + Plane.y * beam.y + Plane.z * beam.z;
+                let t = num / (-den);
+                let point = [A.x + t * beam.x, A.y + t * beam.y, A.z + t * beam.z];
+                return point;
+            }
+        constructor(x, y, z) 
+        {
+            if(typeof(y) === 'undefined')
+            {
+                if(typeof(x.x) === 'undefined')
+                {
+                this.x = x[0],//[0],
+                this.y = x[1],//[1],
+                this.z = x[2];//[2];   
+                }
+                else
+                {
+                this.x = x.x,//[0],
+                this.y = x.y,//[1],
+                this.z = x.z;//[2];       
+                }
+            } 
+            else
+            {
+            this.x = x,
+            this.y = y,
+            this.z = z
+            };
+        }
+        set(x, y, z)
+        {
+            this.x = x,
+            this.y = y,
+            this.z = z;
+        }
+        shift(P)
+        {
+            return new Vertex(this.x - P.x, this.y - P.y, this.z - P.z);
+        }
+        pan(x, y, z)
+        {
+            this.set(this.x + x, this.y + y, this.z + z);
+        }
+        add(V)
+        {
+            this.set(this.x + V.x, this.y + V.y, this.z + V.z);
+        }
+
+        subtract(V)
+        {
+            this.set(this.x - V.x, this.y - V.y, this.z - V.z);    
+        }
+        scalar(x, y, z){
+            return new Vertex(
+            x * this.x,
+            y * this.y,
+            z * this.z
+            );
+        }
+        /*
+        scalar(v){
+            this.set(this.x * v, this.y * v, this.z * v);
+        }*/
+        rotX(theta)
+        {
+            var x = this.x;
+            var y = this.y;
+            var z = this.z;
+            return new Vertex(
+                x,
+                Math.cos(theta) * y - Math.sin(theta) * z,
+                Math.sin(theta) * y + Math.cos(theta) * z
+            );
+        }
+        rotY(theta)
+        {
+            var x = this.x;
+            var y = this.y;
+            var z = this.z;
+            return new Vertex(
+                Math.cos(theta) * x - Math.sin(theta) * z,
+                y,
+                Math.sin(theta) * x + Math.cos(theta) * z
+            );
+        }
+        rotZ(theta){
+            var x = this.x;
+            var y = this.y;
+            var z = this.z;
+            return new Vertex(
+                Math.cos(theta) * x - Math.sin(theta) * y,
+                Math.sin(theta) * x + Math.cos(theta) * y,
+                z
+            );
+        }  
+        project(depth){
+        return new Verticle(this.x/depth,this.y/depth);
+        }
+    }
